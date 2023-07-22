d80ae309fb903e67a0e457a02826454876881c1b
pranay2508 <bhatnagarpranay8@gmail.com>
2023-07-22 22:12:22 +0530

    .

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -12,8 +12,14 @@ const Cart = (props) => {
   console.log(totalAmount)
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
-  const cartItemAddHandler = (item) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+    };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item,amount:1});
+
+
+  };
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
