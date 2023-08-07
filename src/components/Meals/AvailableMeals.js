a63d65ca2d0a25aa7ed407b21c1b7bb80af28b28
pranay2508 <bhatnagarpranay8@gmail.com>
2023-08-07 21:04:35 +0530

    firebase added for fetch

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,35 +1,31 @@
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import {useEffect ,useState} from 'react';
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals , setMeals] =useState([]);
+  useEffect( () =>{
+    const fetchMeals = async () =>{
+     const response =  await fetch('https://food-order-f6f90-default-rtdb.firebaseio.com/Meals.json');
+    const responseData = await response.json();
+    const loadedMeals =[];
+    for (const key in responseData){
+      loadedMeals.push({
+        id:key,
+        name:responseData[key].name,
+        description:responseData[key].name,
+        price:responseData[key].price,
+      })
+    }
+    setMeals(loadedMeals);
+    };
+    fetchMeals();
+    },[ ])
+    //we have to add .json specifcially for use real time database in firebase.
+  //fetch return a promise because sending http request is asynchronous task 
+  //effect function is syncronous we have to use async inside the function 
+    const mealsList = meals.map((meal) => (
     <MealItem
      id={meal.id}
       key={meal.id}
